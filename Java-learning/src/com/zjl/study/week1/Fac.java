7fbceafe6027d6a88fc5007c78e62412b659fab4
zhengjiali6 <1239284041@qq.com>
2020-10-05 21:29:33 +0800

    java课程学习

---
+++ Java-learning/src/com/zjl/study/week1/Fac.java
@@ -0,0 +1,13 @@
+package com.zjl.study.week1;
+
+/**
+ * @ClassName Fac
+ * @Description TODO
+ * @Author zjl
+ * @Date 2020/10/5
+ **/
+public class Fac {
+    public static void main(String[] args) {
+        
+    }
+}
