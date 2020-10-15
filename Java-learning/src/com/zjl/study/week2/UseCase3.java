55351d08fbce7562c9277903e06a0a29e0984967
zhengjiali6 <71875291+zhengjiali6@users.noreply.github.com>
2020-10-15 18:25:16 +0800

    Add files via upload

---
+++ Java-learning/src/com/zjl/study/week2/UseCase3.java
@@ -0,0 +1,23 @@
+package com.zjl.study.week2;
+
+/**
+ * @ClassName UseCase3
+ * @Description TODO
+ * @Author zjl
+ * @Date 2020/10/15
+ **/
+public class UseCase3 {
+    private float height;
+    private float width;
+    public UseCase3(float height,float width){
+        this.height=height;
+        this.width=width;
+    }
+    public float square(){
+        return height*width;
+    }
+    public static void main(String args[]){
+        UseCase3 u=new UseCase3(3.5f,4.5f);
+        System.out.println(u.square());
+    }
+}
